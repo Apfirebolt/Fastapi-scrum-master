ee18ba8d1b6a43270b522706b438181a16c13e66
Apfirebolt <aspper20@gmail.com>
2023-10-09 01:13:23 +0530

    feat: added framer motion and loader styles updated

--- frontend/src/pages/Scheduler.jsx
+++ frontend/src/pages/Scheduler.jsx
@@ -81,6 +81,7 @@ const Scheduler = () => {
 
   return (
     <div className="bg-gray-50">
+      <Loader />
       <div className="mx-auto max-w-screen-xl px-4 py-16 lg:flex lg:items-center">
         <div className="mx-auto max-w-xl text-center">
           <h1 className="text-3xl font-extrabold sm:text-5xl">Scheduler</h1>
