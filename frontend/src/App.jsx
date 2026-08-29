1677d618786389491930afb2861393e70fb56f75
Apfirebolt <aspper20@gmail.com>
2026-08-30 02:04:54 +0530

    toast css changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,39 @@
+import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
-import AnimatedRoutes from "./components/AnimatedRoutes";
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer } from "react-toastify";
 
-import Footer from "./components/Footer";
 import Header from "./components/Header";
-
+import Footer from "./components/Footer";
+import AnimatedRoutes from "./components/AnimatedRoutes";
 
 const App = () => {
   return (
     <Router>
-      <Header />
-      <AnimatedRoutes />
-      <Footer />
-      <ToastContainer />
+      <div className="flex min-h-screen flex-col bg-slate-50 text-slate-900 antialiased">
+        <Header />
+
+        {/* Main Content Area: flex-grow ensures footer stays at the bottom */}
+        <main className="flex-1">
+          <AnimatedRoutes />
+        </main>
+
+        <Footer />
+
+        {/* Global Toast Notifications */}
+        <ToastContainer
+          position="top-right"
+          autoClose={3500}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+          limit={4}
+        />
+      </div>
     </Router>
   );
 };
